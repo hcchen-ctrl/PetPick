98eb45fc492f66d5b86d2aa527bafecb65af5a9b
陳虹妗 <hc.chen24@gmail.com>
2025-08-11 23:25:10 +0800

    2508112324

---
+++ src/test/java/com/petpick/petpick/PetPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petpick.petpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
