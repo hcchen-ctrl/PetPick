9a4e2bcc51c81ba8328c1e2494af7d969c0cd1b5
hcchen-ctrl <hc.chen24@gmail.com>
2025-08-12 14:25:52 +0800

    2508121425

--- src/main/java/com/petpick/petpick/repository/userRepository.java
+++ src/main/java/com/petpick/petpick/repository/userRepository.java
@@ -5,4 +5,6 @@ import com.petpick.petpick.entity.userEntity;
 
 public interface userRepository extends JpaRepository<userEntity,Long> {
     boolean existsByUsername(String username);
-}
+userEntity findByUsername(String username);
+
+   }
