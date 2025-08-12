0f2f925c1fa783a756828393601f0c8cf9cc1261
hcchen-ctrl <hc.chen24@gmail.com>
2025-08-12 16:43:26 +0800

    2508121643完成註冊資料匯入資料庫

---
+++ src/main/java/com/petpick/petpick/DTO/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.petpick.petpick.DTO;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    // Getter 和 Setter
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
